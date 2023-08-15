bc2c9555cbc6433ea602e757c94ff2775e337ddd
Alexander <33152397+boumer7@users.noreply.github.com>
2023-08-15 22:23:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def download_video():
 def download_subtitles():
     video_link = request.args.get('link')
     lang = request.args.get('lang', 'en')  # Default to English if not provided
-    output_format = request.args.get('format', 'srt')  # Default to SRT format if not provided
+    output_format = request.args.get('format', 'json')  # Default to SRT format if not provided
     start_time = request.args.get('start_time')  # Start time for subtitles extraction (in HH:MM:SS format)
     end_time = request.args.get('end_time')  # End time for subtitles extraction (in HH:MM:SS format)
 
