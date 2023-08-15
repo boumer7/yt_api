601ae752585a05a60ad2660257f722ea6db485f9
boumer7 <boumer@yahoo.com>
2023-08-15 14:42:28 +0000

    v2

--- app.py
+++ app.py
@@ -1,19 +1,80 @@
+import os
+import hmac
+import hashlib
+from subprocess import run
 from flask import Flask, request, jsonify
-import subprocess
+import yt_dlp
 
 app = Flask(__name__)
 
-@app.route('/download', methods=['POST'])
-def download():
+# Retrieve the secret token from the environment variable
+SECRET_TOKEN = os.environ.get('SECRET_TOKEN')
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # Validate the incoming request using the secret token
+    signature = request.headers.get('X-Hub-Signature')
+    if not is_valid_signature(request.data, signature, SECRET_TOKEN):
+        return "Unauthorized", 401
+
+    # Parse the payload JSON
+    payload = request.json
+
+    # Check if it's a push event
+    if 'push' in payload.get('event', ''):
+        # Run the deployment script
+        run(["bash", "deploy.sh"])
+
+        return "Webhook received and deployment triggered."
+
+    return "Webhook received, but no action taken."
+
+def is_valid_signature(data, signature, secret):
+    calculated_signature = 'sha1=' + hmac.new(secret.encode(), data, hashlib.sha1).hexdigest()
+    return hmac.compare_digest(calculated_signature, signature)
+
+@app.route('/download_audio', methods=['POST'])
+def download_audio():
     data = request.json
-    url = data['url']
-    output_format = data.get('format', 'best')
+    video_url = data.get('video_url')
 
     try:
-        subprocess.run(['yt-dlp', '-f', output_format, '-o', 'output.%(ext)s', url], check=True)
-        return jsonify({"status": "success", "message": "Download complete!"})
-    except subprocess.CalledProcessError as e:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'extractaudio': True,
+            'audioformat': 'mp3',
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(video_url, download=True)
+            audio_filepath = f"downloads/{info_dict['title']}.mp3"
+
+        return jsonify({"status": "success", "message": "Audio downloaded successfully", "audio_filepath": audio_filepath})
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
+@app.route('/download_video', methods=['POST'])
+def download_video():
+    data = request.json
+    video_url = data.get('video_url')
+
+    try:
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(video_url, download=True)
+            video_filepath = f"downloads/{info_dict['title']}.mp4"
+
+        return jsonify({"status": "success", "message": "Video downloaded successfully", "video_filepath": video_filepath})
+
+    except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
+
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pull latest changes from the repository
+git pull
+
+# Rebuild the Docker image
+docker build -t yt-api .
+
+# Stop and remove the existing container
+docker stop yt-api-container
+docker rm yt-api-container
+
+# Run the updated container
+docker run -d --name yt-api-container -p 5000:5000 yt-api
